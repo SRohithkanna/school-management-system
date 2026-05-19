b52e339f328fe21032b36542a4a6cfa6de34f80b
SRohithkanna <srohithkanna@gmail.com>
2026-05-19 13:38:03 +0530

    updated app.js with routes to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Home          from './pages/Home';
+import Dashboard     from './pages/Dashboard';
+import StudentList   from './pages/StudentList';
+import AddStudent    from './pages/AddStudent';
+import StudentDetail from './pages/StudentDetail';
+import './index.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="page-wrapper">
+        <Sidebar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/"                    element={<Home />} />
+            <Route path="/dashboard"           element={<Dashboard />} />
+            <Route path="/students"            element={<StudentList />} />
+            <Route path="/add-student"         element={<AddStudent />} />
+            <Route path="/students/:id"        element={<StudentDetail />} />
+            <Route path="/students/:id/edit"   element={<AddStudent />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
