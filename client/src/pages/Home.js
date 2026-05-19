365290e2744be171cfe13f1037c20af0428eb776
SRohithkanna <srohithkanna@gmail.com>
2026-05-19 13:52:45 +0530

    remove unused library for deployment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MdPeople, MdPersonAdd, MdDashboard, MdSchool, MdStar } from 'react-icons/md';
+import { MdPeople, MdPersonAdd, MdDashboard, MdStar } from 'react-icons/md';
 import './Home.css';
 
 const features = [
